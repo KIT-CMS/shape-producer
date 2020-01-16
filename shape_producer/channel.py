8c66aee0528b6284b65b9db08acd3e2bfa096b88
Maximilian Burkart <maxiburkart@gmail.com>
2020-01-16 14:18:05 +0100

    Switch to PUPPI mt in em channel cut.

--- shape_producer/channel.py
+++ shape_producer/channel.py
@@ -452,7 +452,7 @@ class EMSM2016(Channel):
             Cut("dilepton_veto<0.5", "dilepton_veto"),
             Cut("iso_1<0.15", "ele_iso"), Cut("iso_2<0.2", "muon_iso"),
             Cut("q_1*q_2<0", "os"),
-            Cut("nbtag==0 && mTdileptonMET<60","bveto_mTdileptonMET"),
+            Cut("nbtag==0 && mTdileptonMET_puppi<60","bveto_mTdileptonMET"),
             Cut("pt_2>10 && ((pt_1>13 && pt_2>24 && trg_muonelectron_mu23ele12 == 1) || (pt_1>24 && pt_2>10 && trg_muonelectron_mu8ele23 == 1))","trg_selection"))
 
 class EMMSSM2016(Channel):
@@ -491,7 +491,7 @@ class EMSM2017(Channel):
             Cut("dilepton_veto<0.5", "dilepton_veto"),
             Cut("iso_1<0.15", "ele_iso"), Cut("iso_2<0.2", "muon_iso"),
             Cut("q_1*q_2<0", "os"),
-            Cut("nbtag==0 && mTdileptonMET<60","bveto_mTdileptonMET"),
+            Cut("nbtag==0 && mTdileptonMET_puppi<60","bveto_mTdileptonMET"),
             Cut("pt_2>10 && ((trg_muonelectron_mu23ele12 == 1) || (trg_muonelectron_mu8ele23 == 1))",
                 "trg_selection"))
 
@@ -505,7 +505,7 @@ class EMSM2018(Channel):
             Cut("dilepton_veto<0.5", "dilepton_veto"),
             Cut("iso_1<0.15", "ele_iso"), Cut("iso_2<0.2", "muon_iso"),
             Cut("q_1*q_2<0", "os"),
-            Cut("nbtag==0 && mTdileptonMET<60","bveto_mTdileptonMET"),
+            Cut("nbtag==0 && mTdileptonMET_puppi<60","bveto_mTdileptonMET"),
             Cut("(trg_muonelectron_mu23ele12 == 1 && pt_1>13 && pt_2 > 24) || (trg_muonelectron_mu8ele23 == 1 && pt_1>24 && pt_2>10)",
                 "trg_selection"))
 
