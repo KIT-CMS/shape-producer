bc716f8cb3f40e10160e0e16841f02f415edf69f
Maximilian Burkart <maxiburkart@gmail.com>
2020-01-30 11:52:51 +0100

    Remove hotfix for tau id weights in embedded samples
    (cherry picked from commit b569cad41bc35470c15b6726dde0d068f9549618)
    Signed-off-by: Olena Hlushchenko <greyxray@gmail.com>

--- shape_producer/estimation_methods_2016.py
+++ shape_producer/estimation_methods_2016.py
@@ -1178,7 +1178,8 @@ class ZTTEmbeddedEstimation(EstimationMethod):
             # self.embedding_tauid(), # NOT IN THE DEEPTAU, are the  get_tauByIsoIdWeight_for_channel calling the right numbers?
             emb_weights.add(self.get_tauByIsoIdWeight_for_channel(self.channel))
             # is this valid for MVA?
-            emb_weights.add(Weight("(pt_2<=20)*1.0+(pt_2>20&&pt_2<=25)*0.97+(pt_2>25&&pt_2<=30)*0.96+(pt_2>30&&pt_2<=35)*0.97+(pt_2>35&&pt_2<=40)*0.98+(pt_2>40)*0.94", "emb_tauID_correction_weight"))
+            # emb_weights.add(Weight("(pt_2<=20)*1.0+(pt_2>20&&pt_2<=25)*0.97+(pt_2>25&&pt_2<=30)*0.96+(pt_2>30&&pt_2<=35)*0.97+(pt_2>35&&pt_2<=40)*0.98+(pt_2>40)*0.94", "emb_tauID_correction_weight"))
+
             emb_weights.add(
                 Weight("gen_match_1==4 && gen_match_2==5", "emb_veto"))
             emb_weights.add(self.emb_triggerweights())
@@ -1187,7 +1188,7 @@ class ZTTEmbeddedEstimation(EstimationMethod):
             emb_weights.add(Weight("idWeight_1*isoWeight_1", "lepton_sf"))
             emb_weights.add(self.get_tauByIsoIdWeight_for_channel(self.channel)),
             # is this valid for MVA?
-            emb_weights.add(Weight("(pt_2<=20)*1.0+(pt_2>20&&pt_2<=25)*0.97+(pt_2>25&&pt_2<=30)*0.96+(pt_2>30&&pt_2<=35)*0.97+(pt_2>35&&pt_2<=40)*0.98+(pt_2>40)*0.94", "emb_tauID_correction_weight"))
+            # emb_weights.add(Weight("(pt_2<=20)*1.0+(pt_2>20&&pt_2<=25)*0.97+(pt_2>25&&pt_2<=30)*0.96+(pt_2>30&&pt_2<=35)*0.97+(pt_2>35&&pt_2<=40)*0.98+(pt_2>40)*0.94", "emb_tauID_correction_weight"))
             emb_weights.add(
                 Weight("gen_match_1==3 && gen_match_2==5", "emb_veto"))
             emb_weights.add(self.emb_triggerweights())
@@ -1195,9 +1196,10 @@ class ZTTEmbeddedEstimation(EstimationMethod):
         elif self.channel.name == "tt":
             emb_weights.add(self.emb_triggerweights())
             emb_weights.add(self.get_tauByIsoIdWeight_for_channel(self.channel))
+
             # is this valid for MVA?
-            emb_weights.add(Weight("((decayMode_1==0)*0.92+(decayMode_1==1)*0.96+(decayMode_1==10)*0.98+(decayMode_1==11)*0.86)", "emb_tauID_correction_weight1"))
-            emb_weights.add(Weight("((decayMode_2==0)*0.92+(decayMode_2==1)*0.96+(decayMode_2==10)*0.98+(decayMode_2==11)*0.86)", "emb_tauID_correction_weight2"))
+            # emb_weights.add(Weight("((decayMode_1==0)*0.92+(decayMode_1==1)*0.96+(decayMode_1==10)*0.98+(decayMode_1==11)*0.86)", "emb_tauID_correction_weight1"))
+            # emb_weights.add(Weight("((decayMode_2==0)*0.92+(decayMode_2==1)*0.96+(decayMode_2==10)*0.98+(decayMode_2==11)*0.86)", "emb_tauID_correction_weight2"))
             emb_weights.add(
                 Weight("gen_match_1==5 && gen_match_2==5", "emb_veto"))
         elif self.channel.name == "em":
